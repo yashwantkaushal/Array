db4dfd414a5d2375619bb47d6c2c530a3a979919
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:22:15 +0530

    Add files via upload

---
+++ ArraysIntro.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class ArraysIntro {
+
+	public static void main(String[] args) {
+		
+		
+		
+//		int [] age = new int[5];      way of writing syntax
+//		
+//		int[] age ;                    OR
+//		age = new int[10];
+//		
+//		int marks[]= new int[15];   way of accessing arrays
+		
+//		System.out.println(marks[1]);
+		
+//		int[] age = {1, 2, 5, 10, 70};
+//		double[] marks = {1.0,3.14,2.15};  initalization 
+//		marks[1]=4.56; update the value
+//		
+//		System.out.println(marks[1]);
+		
+		int[] age = {2,3,10,6,7,90};
+		
+		for(int i = 0; i<age.length; i++) {
+//			find the length of array we use array name.length
+			System.out.println(age[i]);
+			
+		} 		
+				
+
+	}
+
+}
+
